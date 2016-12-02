deb5719615f1c72dca0fc1a8061132de45dc481b
John Nolan <jnolan5@nd.edu>
2016-12-01 21:55:30 -0500

    Can very roughly translate between the moon and back

--- test2.js
+++ test2.js
@@ -168,26 +168,31 @@ window.onload = function init(){
 	console.log(object.position)
 	
 	// set new origin
-	if (object.position.distanceTo(mOrigin) < gravField + mRadius) {
-			object.lookAt(new THREE.Vector3(mOrigin)) // to do, make this smoother with a lerp and make sure it goes on center
+	if (!originObj.equals(mOrigin) && object.position.distanceTo(mOrigin) < gravField + mRadius) {
+			// point towards moon
+			object.lookAt(new THREE.Vector3(mOrigin)) // to do: make this smoother with a lerp and make sure it goes on center
 			object.position.set(mOrigin)
 			object.rotation.y = Math.PI/2
 			upV = 0.0
 			originObj = new THREE.Vector3(mOrigin.x, mOrigin.y, mOrigin.z)
 			radius = mRadius*5 // I have to think through how the radius switching should work, so far the results are bad
 			radObj = mRadius*5 + gravField
-			object.quaternion.set(0, 0, 0, 1).normalize()
+			object.quaternion.set(0, 0, 0, 1).normalize() // set quaternion back to identity
 			console.log("hit moon")
 			console.log(object.position)
 	}
-//	if (object.position.distanceTo(eOrigin) < gravField + eRadius) {
-//		object.lookAt(new THREE.Vector3(eOrigin))
-//		object.rotation.y = Math.Pi/2
-//		upV = 0.0
-//		originObj = new THREE.Vector3(eOrigin.x, eOrigin.y, eOrigin.z)
-//		radius = eRadius
-//		radObj = eRadius + gravField
-//	}
+			console.log(originObj != eOrigin)
+			console.log(originObj)
+			console.log(eOrigin)
+	if (!originObj.equals(eOrigin) && object.position.distanceTo(eOrigin) < eRadius) {
+		console.log(originObj != eOrigin)
+		object.lookAt(new THREE.Vector3(eOrigin))
+	//	object.rotation.y = Math.Pi/2
+		upV = 0.0
+		originObj = new THREE.Vector3(eOrigin.x, eOrigin.y, eOrigin.z)
+		radius = eRadius
+		radObj = eRadius + gravField
+	}
 	
 	if (rotVx < -0.005) {
 	rotVx += 0.002
