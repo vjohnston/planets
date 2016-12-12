bcfc30edf0d0b147e2f7b04ffb7668350911563a
victoriajohnston <1victoriajohnston@gmail.com>
2016-12-12 14:40:55 -0500

    major clean up

--- test2.js
+++ test2.js
@@ -1,93 +1,85 @@
 "use strict";
 var scene;
-var cameraFol; // follow character
-var cameraLook; // look from a distance
+var cameraFol; 			// follow character
+var cameraLook; 		// look from a distance
+var windowWidth, windowHeight;	// window size
+var renderer; 			// for updating views
 
+// character
+var xpos=0.0099;		// x position of object
+var ypos=0.0; 			// y position of object
+var zpos=0.53;			// z position of object
+var character;			// character that user can control
+var object;				// body of the character
+var feet;				// characters two feet
+var horizontal = true;	// keeps track which direction character is moving
+var originObj = new THREE.Vector3(0, 0, 0);	// object original position
 
-var xpos=0.0099;	// x position of object
-var ypos=0.0; 		// y position of object
-var zpos=0.53;		// z position of object
-var eRadius = 0.55;	// radius of earth
-var mRadius = 0.65/5; // scaled down by 5, but that seems too small
-var fRadius = 0.16; // just for fun
-var radObj = 0.53; // the radial position of the object (for the jumping)
-var radius = eRadius;
+// planets
+var eRadius = 0.55;		// radius of earth
+var mRadius = 0.13; 	// radius of moon scaled down by 5 form earth
+var fRadius = 0.16; 	// just for fun
+var radObj = eRadius; 		// the radial position of the object (for the jumping)
+var radius = eRadius;	// current radius
+var eOrigin = new THREE.Vector3(0,0,0);			// origin of earth
+var mOrigin = new THREE.Vector3(0.5, 0.5, 0.5);	// origin of moon
+var fOrigin = new THREE.Vector3(0.7, -0.7, 0.7);// origin of fun
 
-var object;		// object traversing planets
-var originObj = new THREE.Vector3(0, 0, 0);
-var eOrigin = new THREE.Vector3(0,0,0);
-var mOrigin = new THREE.Vector3(0.5, 0.5, 0.5);
-var fOrigin = new THREE.Vector3(0.7, -0.7, 0.7);
-
+// speed and raycaster
 var raycaster = new THREE.Raycaster();	// set up ray caster
-var earthMesh;
-var rotVx = 0.0; // rotational velocity around the current planet, left and right
-var rotVy = 0.0;
-var upV = 0.0;
-var gravField = 0.08;
+var rotVx = 0.0; 		// rotational velocity around the current planet, left and right
+var rotVy = 0.0;		// rotational velocity around the current planet, up and down 
+var upV = 0.0;			// up velocity 
+var gravField = 0.08;	// gravitational fireld 
 
-//arrays to loop through for gravity pull check
-var radii = [eRadius, mRadius, fRadius];
-var origins = [eOrigin, mOrigin, fOrigin];
-var numPlanets = 3;
+// planet arrays
+var radii = [eRadius, mRadius, fRadius];	// planet radii 
+var origins = [eOrigin, mOrigin, fOrigin];	// planet origins
+var textures = ['threex_textures/earthmap1k.jpg','threex_textures/moonmap1k.jpg','threex_textures/moonmap1k.jpg'];
+var bumpMaps = ['threex_textures/earthbump1k.jpg','threex_textures/moonbump1k.jpg','threex_textures/moonbump1k.jpg'];
+var numPlanets = 3;							// number of planets
 
-//var onPlanetA = 1.0;
-//var originLast
-//var originNext
-//var lookVector = new THREE.Vector3(0, 0, 0)
 
-var character;
-var feet;
-
-var windowWidth, windowHeight;
-
-var horizontal = true;
-
-var renderer; // for updating views
-
-//Views:
-
+// views for the two screens
 var views = [
-{
-						 left:0,
-						 bottom:0,
-						 width: 0.5,
-						 height: 1.0,
-						 background: new THREE.Color().setRGB(0.0, 0.0, 0.0), // TODO: set start background
-//						 eye: [0, 300, 1800 ],
-//						 up: [0, 1, 0],
-						 fov: 45,
-						 updateCamera: function ( camera, scene, position ) {
-							
-						 }
-},
-{
-						 left: 0.5,
-						 bottom: 0,
-						 width: 0.5,
-						 height: 1.0,
-						 background: new THREE.Color().setRGB(0.0, 0.0, 0.0),
-//						 up: [0, 1, 0],
-						 fov: 45,
-						 updateCamera: function( camera, scene, position) {
-							camera.lookAt(position)
-						 }
-}
+	{
+		left:0,
+		bottom:0,
+		width: 0.5,
+		height: 1.0,
+		background: new THREE.Color().setRGB(0.0, 0.0, 0.0),
+		fov: 45,
+		updateCamera: function ( camera, scene, position ) {}
+	},
+	{
+		left: 0.5,
+		bottom: 0,
+		width: 0.5,
+		height: 1.0,
+		background: new THREE.Color().setRGB(0.0, 0.0, 0.0),
+		fov: 45,
+		updateCamera: function( camera, scene, position) {
+			camera.lookAt(position)
+		}
+	}
 ]
+
+// on load
 window.onload = function init(){
-	// set up renderer
+	// set up renderer- set up size and add in to the document
 	renderer = new THREE.WebGLRenderer({
 		antialias : true
 	});
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	document.body.appendChild( renderer.domElement );
 	renderer.shadowMap.Enabled = true
-	var onRenderFcts= [];
 
+	// used to continuously render
+	var onRender = [];
 
-	// set up scene and cameras
+	// set up scene and both cameras
 	scene	= new THREE.Scene();
-	cameraFol	= new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 100 );
+	cameraFol = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 100 );
 	cameraLook	= new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.01, 100 );
 	cameraLook.position.z = 3;
 	cameraFol.position.z = 3;
@@ -95,7 +87,7 @@ window.onload = function init(){
 	// add ambient lighting
 	var ambientLight = new THREE.AmbientLight( 0x222222, 1)
 	scene.add( ambientLight )
-  // add directional lighting
+  	// add directional lighting
 	var dirLight = new THREE.DirectionalLight( 0xaaaaaa, 1 )
 	dirLight.position.set(5,5,5)
 	scene.add( dirLight )
@@ -109,72 +101,30 @@ window.onload = function init(){
 	var backgroundGeometry	= new THREE.SphereGeometry(100, 32, 32)
 	var background	= new THREE.Mesh(backgroundGeometry, backgroundMaterial)
 	scene.add(background)
+
+
+	// set up all the planets
+	for (var i = 0; i < numPlanets; i++) {
+		var geometry = new THREE.SphereGeometry(radii[i], 32, 32)
+		var material = new THREE.MeshPhongMaterial({
+			map	: THREE.ImageUtils.loadTexture(textures[i]),
+			bumpMap	: THREE.ImageUtils.loadTexture(bumpMaps[i]),
+			bumpScale: 0.05,
+		})
+		var planet = new THREE.Mesh(geometry, material)
+		planet.position.set(origins[i].x, origins[i].y, origins[i].z)
+		planet.receiveShadow = true
+		planet.castShadow = true
+		scene.add(planet)
+	}
 	
-	// create earth container
-	var container	= new THREE.Object3D()
-	container.rotateZ(-23.4 * Math.PI/180)
-	container.position.z	= 0
-	scene.add(container)
-
-	// create the earth
-	var earthGeometry = new THREE.SphereGeometry(0.5, 32, 32)
-	earthGeometry.computeFaceNormals();
-	var earthMaterial = new THREE.MeshPhongMaterial({
-		map		: THREE.ImageUtils.loadTexture('threex_textures/earthmap1k.jpg'),
-		bumpMap		: THREE.ImageUtils.loadTexture('threex_textures/earthbump1k.jpg'),
-		bumpScale	: 0.05,
-		specularMap	: THREE.ImageUtils.loadTexture('threex_textures/earthspec1k.jpg'),
-		specular	: new THREE.Color('grey'),
-	})
-	var earth = new THREE.Mesh(earthGeometry, earthMaterial)
-	earth.receiveShadow	= true
-	earth.castShadow	= true
-	container.add(earth)
-	onRenderFcts.push(function(delta, now){
-		earth.rotation.y += 1/32 * delta;		
-	})
-
-	// set up clouds for earth
-	var clouds	= setUpClouds()
-	clouds.receiveShadow = true
-	clouds.castShadow = true
-	container.add(clouds)
-	onRenderFcts.push(function(delta, now){
-		clouds.rotation.y += 1/8 * delta;		
-	})
-
-	// create moon and add to scene
-	var moonGeometry = new THREE.SphereGeometry(0.5, 32, 32)
-	var moonMaterial = new THREE.MeshPhongMaterial({
-		map	: THREE.ImageUtils.loadTexture('threex_textures/moonmap1k.jpg'),
-		bumpMap	: THREE.ImageUtils.loadTexture('threex_textures/moonbump1k.jpg'),
-		bumpScale: 0.002,
-	})
-	var moon = new THREE.Mesh(moonGeometry, moonMaterial)
-	moon.scale.multiplyScalar(1/5)
- 	moon.position.set(0.5,0.5,0.5)
-	moon.receiveShadow	= true
-	moon.castShadow	= true
-	scene.add(moon)
-	
-	var funGeometry = new THREE.SphereGeometry(fRadius, 24, 24)
-	var funMaterial = new THREE.MeshPhongMaterial({
-																								map	: THREE.ImageUtils.loadTexture('threex_textures/moonmap1k.jpg'),
-																								bumpMap	: THREE.ImageUtils.loadTexture('threex_textures/moonbump1k.jpg'),
-																								bumpScale: 0.002,
-																								})
-	var fun = new THREE.Mesh(funGeometry, funMaterial)
-	fun.position.set(fOrigin.x, fOrigin.y, fOrigin.z)
-	fun.receiveShadow	= true
-	fun.castShadow	= true
-	scene.add(fun)
 
 	// character
 	character = new THREE.Object3D()
 	character.position.z = zpos
 	scene.add(character)
 
-	// create another object
+	// create object - character body
 	var objectGeometry	= new THREE.SphereGeometry(0.1, 32, 32)
 	var objectMaterial	= new THREE.MeshPhongMaterial({
 		map	: THREE.ImageUtils.loadTexture('textures/planet.jpg'),
@@ -192,26 +142,18 @@ window.onload = function init(){
 	object.scale.multiplyScalar(1/5)
 	object.receiveShadow	= true
 	object.castShadow	= true
-	//scene.add(object)
 	character.add(object)
-  	//camera.position.y = -1
- 	/****comment below to stop camera moving with the object****/
+
+  	// init camera for object
 	object.add(cameraFol);
 	views[0].camera = cameraFol;
 	views[1].camera = cameraLook;
-	//object.add(views[0].camera)
  	views[0].camera.position.y = -2.5
  	views[0].camera.lookAt(object.position)
-
-	//originObj = new THREE.Vector3(origin.x, origin.y, origin.z)
  
-	// feet
+	// character feet
 	var foot = new THREE.SphereGeometry(0.008, 32, 32);
 	feet = new THREE.Object3D();
-	/*feet = {
-		left: new THREE.Mesh(foot, objectMaterial),
-		right: new THREE.Mesh(foot, objectMaterial)
-	};*/
 	var left = new THREE.Mesh(foot, objectMaterial);
 	var right = new THREE.Mesh(foot, objectMaterial);
 	feet.position.setX(0)
@@ -235,132 +177,107 @@ window.onload = function init(){
 	}, false)
 
 
-
+	// update size of window for split screen
 	function updateSize() {
-		
 		if ( windowWidth != window.innerWidth || windowHeight != window.innerHeight ) {
-			
 			windowWidth  = window.innerWidth;
 			windowHeight = window.innerHeight;
-			
 			renderer.setSize ( windowWidth, windowHeight );
+		}
+	}
+
+
+
+	// push onto onRender to render - rendering functoin
+	onRender.push(function(delta, now){
+
+		// update window size
+		updateSize();
+
+		// Update views
+		for (var i = 0; i < views.length; i++) {
+			// update camera for view
+			var view = views[i];
+			var camera = view.camera;
+			view.updateCamera( camera, scene, object.position);
+			// set dimensions
+			var left   = Math.floor( windowWidth  * view.left );
+			var bottom = Math.floor( windowHeight * view.bottom );
+			var width  = Math.floor( windowWidth  * view.width );
+			var height = Math.floor( windowHeight * view.height );
+			renderer.setViewport( left, bottom, width, height );
+			renderer.setScissor( left, bottom, width, height );
+			renderer.setScissorTest( true );
+			renderer.setClearColor( view.background );
 			
+			camera.aspect = width / height;
+			camera.updateProjectionMatrix();
+			
+			renderer.render( scene, camera );
+		}
+											
+		// quarternion for x axis rotation						
+		if (rotVx != 0) {
+			character.quaternion.multiply(new THREE.Quaternion(0, Math.sin(rotVx*radius/radObj), 0, Math.cos(rotVx*radius/radObj)));
 		}
 		
-	}
-	
-	// ***** CODE BELOW AND CLOUD SET UP TAKEN FROM THREEX.PLANETS **** //
-
-	
-	
-	onRenderFcts.push(function(delta, now){
-
-	updateSize();
-	//Update view:
-	for (var i = 0; i < views.length; i++) {
-		
-										
-		var view = views[i];
-		var camera = view.camera;
-		
-		view.updateCamera( camera, scene, object.position);
-		
-		var left   = Math.floor( windowWidth  * view.left );
-		var bottom = Math.floor( windowHeight * view.bottom );
-		var width  = Math.floor( windowWidth  * view.width );
-		var height = Math.floor( windowHeight * view.height );
-		renderer.setViewport( left, bottom, width, height );
-		renderer.setScissor( left, bottom, width, height );
-		renderer.setScissorTest( true );
-		renderer.setClearColor( view.background );
-		
-		camera.aspect = width / height;
-		camera.updateProjectionMatrix();
-		
-		renderer.render( scene, camera );
-	}
-										
-										
-	if (rotVx != 0) {
-	character.quaternion.multiply(new THREE.Quaternion(0, Math.sin(rotVx*radius/radObj), 0, Math.cos(rotVx*radius/radObj)));
-	}
-	
-	if (rotVy != 0) {
-	character.quaternion.multiply(new THREE.Quaternion(Math.sin(rotVy*radius/radObj), 0, 0, Math.cos(rotVy*radius/radObj)));
-										// the radius/radobj factor makes it closer to linear velocity, at least while jumping
-	}
-
-
-	// to do, make this equations better so we increase the linear velocity rather than radial velocity at a constant speed, that
-										// will make jumping and moving to planets much better
-	var qx = character.quaternion.x;
-	var qy = character.quaternion.y;
-	var qz = character.quaternion.z;
-	var qw = character.quaternion.w;
-	xpos = 2 * (qy * qw + qz * qx) * radObj + originObj.x; // trying to just translate
-	ypos = 2 * (qz * qy - qw * qx) * radObj + originObj.y;
-	zpos = ((qz * qz + qw * qw) - (qx * qx + qy * qy))* radObj + originObj.z;
-	character.position.setX(xpos);
-	character.position.setY(ypos);
-	character.position.setZ(zpos);
-//	console.log(object.position)
-										
-//	if (onPlanetA < 1.0) {
-//	// originObj.lerpVectors(originNext, originLast, onPlanetA)
-//	 //radObj = onPlanetA * radius
-//	 onPlanetA += 0.002
-//	}
-	// set new origin
-	for (var i = 0; i < numPlanets; i++) {
-		if (!originObj.equals(origins[i]) && character.position.distanceTo(origins[i]) < gravField + radii[i]) {
-				upV = 0.0
-				originObj = new THREE.Vector3(origins[i].x , origins[i].y, origins[i].z)
-				radius = radii[i] // I have to think through how the radius switching should work, so far the results are bad
-				radObj = radii[i]// + gravField
-										break;
+		// quarternion for y axis rotation
+		// the radius/radobj factor makes it closer to linear velocity, at least while jumping
+		if (rotVy != 0) {
+			character.quaternion.multiply(new THREE.Quaternion(Math.sin(rotVy*radius/radObj), 0, 0, Math.cos(rotVy*radius/radObj)));
+											
 		}
-										console.log("origin loop: ");
-										console.log(i);
-	}
-										
-										/**** old switch function ****
-	if (!originObj.equals(eOrigin) && character.position.distanceTo(eOrigin) < eRadius) {
-		console.log(originObj != eOrigin)
-		//object.lookAt(new THREE.Vector3(eOrigin))
-		originLast = new THREE.Vector3(mOrigin.x, mOrigin.y, mOrigin.z)
-		originNext = new THREE.Vector3(eOrigin.x, eOrigin.y, eOrigin.z)
-		upV = 0.0
-		originObj = new THREE.Vector3(eOrigin.x, eOrigin.y, eOrigin.z)
-		radius = eRadius
-		radObj = eRadius + gravField
-	} *****/
-	
-	if (rotVx < -0.005) {
-	rotVx += 0.001
-	} else if (rotVx > 0.005) {
-	rotVx -= 0.001
-	} else {
-		rotVx = 0
-	} // clamp down to zero to prevent drifting
-	if (rotVy < -0.005) {
-	rotVy += 0.001
-	} else if (rotVy > 0.005) {
-	rotVy -= 0.001
-	} else {
-	rotVy = 0
-	}
-										
-	if (radObj > radius) {
-		upV -= 0.005 // the downwards
-		radObj += upV
-	}
+
+		// to do, make this equations better so we increase the linear velocity rather than radial velocity at a constant speed, that
+		// will make jumping and moving to planets much better
+		var qx = character.quaternion.x;
+		var qy = character.quaternion.y;
+		var qz = character.quaternion.z;
+		var qw = character.quaternion.w;
+		// calculate new x,y,z positions and update character position
+		xpos = 2 * (qy * qw + qz * qx) * radObj + originObj.x; 
+		ypos = 2 * (qz * qy - qw * qx) * radObj + originObj.y;
+		zpos = ((qz * qz + qw * qw) - (qx * qx + qy * qy))* radObj + originObj.z;
+		character.position.setX(xpos);
+		character.position.setY(ypos);
+		character.position.setZ(zpos);
+
+
+		// go through the array of all planets and see if the character should be pulled in by any of grav fields
+		for (var i = 0; i < numPlanets; i++) {
+			if (!originObj.equals(origins[i]) && character.position.distanceTo(origins[i]) < gravField + radii[i]) {
+					upV = 0.0	// set up velocity to 0
+					originObj = new THREE.Vector3(origins[i].x , origins[i].y, origins[i].z) // update objects new origin to origin of planet
+					radius = radii[i]
+					radObj = radii[i]
+					break;
+			}
+		}
+											
+		// speed up or slow down based on x and velocities
+		if (rotVx < -0.005) {
+			rotVx += 0.001
+		} else if (rotVx > 0.005) {
+			rotVx -= 0.001
+		} else {
+			rotVx = 0
+		} // clamp down to zero to prevent drifting
+		if (rotVy < -0.005) {
+			rotVy += 0.001
+		} else if (rotVy > 0.005) {
+			rotVy -= 0.001
+		} else {
+			rotVy = 0
+		}
+		
+		// update radObj									
+		if (radObj > radius) {
+			upV -= 0.005 // the downwards
+			radObj += upV
+		}
 								
 	})
-	
-	// render scene
-//	onRenderFcts.push(function(){
-//		renderer.render( scene, camera );		
-//	})
+
 
 	// loop runner
 	var lastTimeMsec= null
@@ -372,63 +289,76 @@ window.onload = function init(){
 		var deltaMsec	= Math.min(200, nowMsec - lastTimeMsec)
 		lastTimeMsec	= nowMsec
 		// call each update function
-		onRenderFcts.forEach(function(onRenderFct){
-			onRenderFct(deltaMsec/1000, nowMsec/1000)
+		onRender.forEach(function(onRender){
+			onRender(deltaMsec/1000, nowMsec/1000)
 		})
 	})
 }
 
+
+
+// handle key down event
 window.addEventListener("keydown", function(e){
-  
+	// space - jump
 	if (e.keyCode == 32){
-	 if (radObj <= radius) {
-			 upV = 0.1 // launch upwards
-			 radObj += upV
-	 }
+		if (radObj <= radius) {
+			upV = 0.1 // launch upwards
+			radObj += upV
+		}
 	}
-												
-  if (e.keyCode === 37){ // left
+			
+	// left									
+  	if (e.keyCode === 37){ 
+  		// if not moving horizontal before, turn object
   		if (!horizontal){
   			feet.rotateZ(Math.PI/2);
   		}
   		horizontal = true;
-      if (rotVx >= -0.05)
-          rotVx -= 0.015
-//	    object.quaternion.multiply(new THREE.Quaternion(0, Math.sin(-0.01), 0, Math.cos(-0.01)));
+  		// decrease x velocity
+	    if (rotVx >= -0.05)
+	        rotVx -= 0.015
 	}
 
-	if (e.keyCode === 39) { // right
+	// right 
+	if (e.keyCode === 39) {
+		// if not moving horizontal before, turn object
 		if (!horizontal){
   			feet.rotateZ(Math.PI/2);
   		}
 		horizontal = true;
-      if (rotVx <= 0.05)
-          rotVx += 0.015
-//	    object.quaternion.multiply(new THREE.Quaternion(0, Math.sin(0.01), 0, Math.cos(0.01)));
+		// increase x velocity
+      	if (rotVx <= 0.05)
+        	rotVx += 0.015
 	}
 
-	if (e.keyCode === 38) { // up
+	// up
+	if (e.keyCode === 38) { 
+		// if moving horizontal before, turn object
 		if (horizontal){
 			feet.rotateZ(Math.PI/2);
 		}
 		horizontal = false;
-      if (rotVy >= -0.05)
-				 rotVy -= 0.015;
-//	    object.quaternion.multiply(new THREE.Quaternion(Math.sin(-0.01), 0, 0, Math.cos(-0.01)));
+		// decrease y velocity
+     	if (rotVy >= -0.05)
+			rotVy -= 0.015;
 	}
 
-	if (e.keyCode === 40) { // down
+	// down
+	if (e.keyCode === 40) {
+		// if moving horizontal before, turn object
 		if (horizontal){
 			feet.rotateZ(Math.PI/2);
 		}
 		horizontal = false;
-			if (rotVy <= 0.05)
-        rotVy += 0.015
-//	    object.quaternion.multiply(new THREE.Quaternion(Math.sin(0.01), 0, 0, Math.cos(0.01)));
+		// increase y velocity
+		if (rotVy <= 0.05)
+        	rotVy += 0.015
 	}
-
 });
 
+
+
+// this function is taken from THREEX.planets
 function setUpClouds(){
 	// create destination canvas
 	var canvasResult	= document.createElement('canvas')
